eef0406c6b7211f67ab6b6d71969fa1e72061156
JeanVillacis <jeanpierrevillacis03@gmail.com>
2026-03-24 22:20:00 -0500

    feat: entidad Usuario con roles GESTOR y ASEGURADO

---
+++ ms-authservice/src/main/java/com/taller/semana7/msauthservice/entity/Usuario.java
@@ -0,0 +1,28 @@
+package com.taller.semana7.msauthservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios", schema = "auth_schema")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Rol rol;
+}
